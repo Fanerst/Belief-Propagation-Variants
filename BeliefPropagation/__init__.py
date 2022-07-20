7e1805901d1e4705e466d3e93e9f602e4210e4ec
Feng Pan <panfeng@itp.ac.cn>
2022-07-20 17:05:58 +0800

    BP revised version compatible with models with fields

--- BeliefPropagation/__init__.py
+++ BeliefPropagation/__init__.py
@@ -1,2 +1,2 @@
-from .vanilla_BP import BP_revised, BPofh
+from .vanilla_BP import belief_propagation
 from .loopy_BP import LoopyBeliefPropagation, NeighborSet, min_max, find_r_neighbor_edges, min_max
